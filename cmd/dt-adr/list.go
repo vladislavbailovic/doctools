07295fc755a053d0055bc196590261beb17950a5
vladislav <ve@incsub.com>
2022-11-07 04:11:51 +0100

    Filter list output by status

--- cmd/dt-adr/list.go
+++ cmd/dt-adr/list.go
@@ -34,8 +34,22 @@ func listAdrs(args []string) {
 	if err != nil {
 		dbg.Error("error listing ADRs: %v", err)
 		return
-	} else {
+	}
+
+	if len(args) == 0 {
 		list = all
+	} else {
+		status, err := adr.StatusTypeFromString(args[0])
+		if err != nil {
+			dbg.Error("error filtering ADRs: %v", err)
+			return
+		}
+		for _, data := range all {
+			if len(data.Status) == 0 || data.Status[len(data.Status)-1].Kind != status {
+				continue
+			}
+			list = append(list, data)
+		}
 	}
 
 	if len(list) == 0 {
