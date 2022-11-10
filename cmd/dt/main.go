7f5d85dc6cc82a1474988bcca8044927a90b2185
vladislav <ve@incsub.com>
2022-11-10 06:37:46 +0100

    Implement main tool help

--- cmd/dt/main.go
+++ cmd/dt/main.go
@@ -1,20 +1,45 @@
 package main
 
-import "doctools/pkg/cli"
+import (
+	"doctools/pkg/cli"
+	_ "embed"
+	"os"
+	"path/filepath"
+)
+
+//go:embed resources/help.txt
+var help string
+
+func showHelp() {
+	cli.Say(help)
+}
 
 func main() {
 	if !cli.HasSubcommand() {
-		cli.Say("HALP!")
+		showHelp()
 	} else {
+		root, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			cli.Cry("%v", err)
+			return
+		}
 		switch cli.Subcommand() {
+		case "-h", "--help", "help":
+			showHelp()
 		case "adr":
-			cmd := cli.Run("./dt-adr", cli.SubcommandArgs()...)
+			cmd := cli.Run(filepath.Join(root, "./dt-adr"), cli.SubcommandArgs()...)
+			if err := cmd.Wait(); err != nil {
+				cli.Cry("error executing: %v", err)
+				return
+			}
+		case "rdme", "readme":
+			cmd := cli.Run(filepath.Join(root, "./dt-rdme"), cli.SubcommandArgs()...)
 			if err := cmd.Wait(); err != nil {
 				cli.Cry("error executing: %v", err)
 				return
 			}
 		default:
-			cli.Say("HALP!")
+			showHelp()
 		}
 	}
 }
