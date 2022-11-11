c9a6d1b185a4c73885c4c207e750d14366c31042
vladislav <ve@incsub.com>
2022-11-11 21:28:53 +0100

    Implement changelog command

--- cmd/dt-chglg/main.go
+++ cmd/dt-chglg/main.go
@@ -2,38 +2,64 @@ package main
 
 import (
 	"doctools/pkg/cli"
+	_ "embed"
+	"os"
 )
 
+//go:embed resources/help.txt
+var help string
+
+func showHelp() {
+	cli.Say(help)
+}
+
 func main() {
+	if !cli.HasSubcommand() {
+		showHelp()
+	} else {
+		switch cli.Subcommand() {
+		case "-h", "--help", "help":
+			showHelp()
+		case "new", "init":
+			_, err := os.Stat("CHANGELOG.md")
+			if err == nil && !cli.HasFlag("-f") && !cli.HasFlag("--force") {
+				cli.Cry("CHANGELOG.md already exists")
+				cli.Say("You can forcefully overwrite it, though (-f/--force)")
+				return
+			}
+
+			if err := initChangelog(); err != nil {
+				cli.Cry("%v", err)
+			}
+		case "update", "toc":
+			_, err := os.Stat("CHANGELOG.md")
+			if err != nil {
+				if err := initChangelog(); err != nil {
+					cli.Cry("%v", err)
+				}
+				return
+			}
+			if err := updateChangelog(); err != nil {
+				cli.Cry("%v", err)
+			}
+		default:
+			showHelp()
+		}
+	}
+}
+
+func initChangelog() error {
+	wip := fromRepo()
+	return writeChangelog(wip)
+}
+
+func updateChangelog() error {
 	known := fromFile("CHANGELOG.md")
 	wip := fromRepo()
 	final := known.updateFrom(wip)
-	cli.Say(final.String())
-	// renderChangeset(changeset.changes)
+	return writeChangelog(final)
 }
 
-func renderChangeset(all []changeset) {
-	for _, set := range all {
-		cli.Say("### %s", set.name)
-		if set.hasChanges() {
-			for _, c := range set.changes {
-				cli.Say("\t- %s", c)
-			}
-		} else {
-			cli.Say("\t* No changes *")
-		}
-		cli.Say("")
-	}
-}
-
-func diffChangesets(wip, known []changeset) []changeset {
-	if len(known) > len(wip) {
-		return []changeset{}
-	}
-	diff := len(wip) - len(known)
-	if diff == 0 {
-		return known
-	}
-
-	return wip[:diff]
+func writeChangelog(log changelog) error {
+	return os.WriteFile("CHANGELOG.md", []byte(log.String()), 0622)
 }
